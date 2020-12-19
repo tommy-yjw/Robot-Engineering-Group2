e726f299a4f31b6311bec51c5f3048d05e9a0bf8
qcc823 <74518018+qcc823@users.noreply.github.com>
2020-12-19 23:58:51 +0800

    Add files via upload

---
+++ 20201213/rect.py
@@ -0,0 +1,29 @@
+import turtle as t
+
+class DrawRectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def set(self,X,Y,Width,Height):
+        self.x=X
+        self.y=Y
+        self.width=Width
+        self.height=Height
+
+    def show(self):
+        t.pensize(2)
+        t.penup()
+        t.goto(self.x,self.y)
+        t.pendown()
+        for _ in range(0,2):
+            t.fd(self.width)
+            t.rt(90)
+            t.fd(self.height)
+            t.rt(90)
+        t.done()
+
+object=DrawRectangle(0,0,100,200)
+object.show()
